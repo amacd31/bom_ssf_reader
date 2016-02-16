ac460b9ba78e79aa5a1e632e42dc0b4309bebdf8
Andrew MacDonald <andrew@maccas.net>
2016-02-16 20:51:32 +1100

    Initial commit - Basic class for fetching Seasonal Streamflow Forecasts

---
+++ bom_ssf_reader/__init__.py
@@ -0,0 +1,41 @@
+import requests
+import bom_data_parser as bdp
+
+try:
+    from urlib2 import urlopen
+except:
+    from urllib.request import urlopen
+
+class SSFReader(object):
+
+    def __init__(self):
+        config_url = 'http://www.bom.gov.au/water/ssf/images/site_config.json'
+        self.metadata = requests.get(config_url).json()
+
+        self.station_list = []
+        self.station_meta = {}
+        for station in self.metadata['stations']['features']:
+            station_id = station['properties']['ID']
+
+            self.station_list.append(station_id)
+
+            self.station_meta[station_id] = {
+                'drainage': station['properties']['drainage'],
+                'basin': station['properties']['basin'],
+                'properties': station,
+            }
+
+    def get_site_info(self, awrc):
+        return self.station_meta[awrc]['properties']
+
+    def get_forecast(self, awrc, forecast_date):
+        forecast_url = '{site}/{drainage}/{basin}/fc/{year}/{month:02d}/{awrc}_FC_10_{year}_{month:02d}_table.csv'.format(**{
+            'site': 'http://www.bom.gov.au/water/ssf',
+            'drainage': self.station_meta[awrc]['drainage'],
+            'basin': self.station_meta[awrc]['basin'],
+            'year': forecast_date.year,
+            'month': forecast_date.month,
+            'awrc': awrc
+        })
+
+        return bdp.read_ssf_csv(urlopen(forecast_url))
